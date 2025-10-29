90d3a38c9737fe6db61d1855c5ff4350c4d54508
Karan Malik <karanmallik65@gmail.com>
2025-10-29 21:11:29 +0530

    upload sql files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-update later
+📊 Explore the data job landscape! This project dives into data analyst roles, uncovering 💰 the highest-paying positions, 🔥 the most sought-after skills, and 📈 how strong demand aligns with top salaries in data analytics.
+
+🔍 Curious about SQL queries? Find them here:
---
+++ project_sql/3_top_demanded_skills.sql
@@ -0,0 +1,54 @@
+/*
+What are the most in-demand skills for data analysts?
+- Join job postings to inner join table similar to query 2
+- Identify the top 5 in-demand skills for a data analyst.
+- Focus on all job postings.
+- Why? Retrieves the top 5 skills with the highest demand in the job market, 
+    providing insights into the most valuable skills for job seekers.
+*/
+
+SELECT 
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' 
+    AND job_work_from_home = True 
+GROUP BY
+    skills
+ORDER BY
+    demand_count DESC
+LIMIT 15;
+
+/*
+In 2023, the most important skills for data analysts included SQL and Excel, showing the need for strong basics in data handling and spreadsheets.
+Technical tools like Python, Tableau, and Power BI were also important, highlighting how data storytelling and decision-making rely more on programming and visualization.
+
+RESULTS
+```````````===============```````````
+
+[
+  {
+    "skills": "sql",
+    "demand_count": "7291"
+  },
+  {
+    "skills": "excel",
+    "demand_count": "4611"
+  },
+  {
+    "skills": "python",
+    "demand_count": "4330"
+  },
+  {
+    "skills": "tableau",
+    "demand_count": "3745"
+  },
+  {
+    "skills": "power bi",
+    "demand_count": "2609"
+  }
+]
+*/
